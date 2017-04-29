6450cfd3d35064d61e19150a1af316fe5027e57a
RealCR1 <Wz892477341@outlook.com>
2017-04-29 14:28:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uicustomviews/TitleLayout.java
@@ -0,0 +1,17 @@
+package com.example.uicustomviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2017/4/29.
+ */
+
+public class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context, AttributeSet attrs){
+        super(context,attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+    }
+}
